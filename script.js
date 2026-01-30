1a26030cea8150ecefcd78126036fdc4ca64c2e3
Tiru vardhan <150202554+tiru02@users.noreply.github.com>
2026-01-30 12:16:31 +0530

    behaviour of cards are been adjusted

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 let left = document.querySelector('input[left]')
 let right = document.querySelector('input[right]')
 let mover = document.querySelector('.fixed-container')
-let leftLength = 0
+let length = 0
 left.onclick = () => {
-    leftLength -= 10
-    if (leftLength <= -50) leftLength = 0
-    mover.style.transform = `translateX(${leftLength}%)`
-    console.log(leftLength)
+    length -= 25
+    if (length <= -50) {
+        left.style.display='none'
+        right.style.display='flex'
+    }else{
+        left.style.display='flex'
+        right.style.display='none'
+    }
+    mover.style.transform = `translateX(${length}%)`
+    console.log(length)
 
 }
 right.onclick = () => {
-    leftLength += 10
-    if (leftLength >= 50) leftLength = 0
-    mover.style.transform = `translateX(${leftLength}%)`
-    leftLength += 10
-    console.log('right', leftLength)
+    length += 25
+    if (length >= 50) {
+        left.style.display='flex'
+        right.style.display='none'
+    }else{
+        
+        left.style.display='none'
+        right.style.display='flex'
+    }
+    mover.style.transform = `translateX(${length}%)`
+    length += 10
+    console.log('right', length)
 }
